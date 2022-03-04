16a3c74fe998bf75708dc756ba48345dcfc401d3
student200036 <200036ww@gmail.com>
2022-03-04 10:30:52 +0900

    fadeIn,fadeOut追加

--- animation/animation.js
+++ animation/animation.js
@@ -2,22 +2,29 @@
 var monster = $('monster')
 console.log(monster)
 function show() {
+    monster.fadeIn()
 }
 
 function hide() {
+    monster.fadeOut()
 }
 
 function moveRight() {
+
 }
 
 function moveLeft() {
+
 }
 
 function moveUp() {
+
 }
 
 function moveDown() {
+
 }
 
 function moveSequence() {
+    
 }
