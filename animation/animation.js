0f5b4b201733608c9b3b300cf25ce6dee4c14e59
student200036 <200036ww@gmail.com>
2022-03-04 10:45:44 +0900

    プログラム鵜の追加と修正

--- animation/animation.js
+++ animation/animation.js
@@ -2,27 +2,27 @@
 var monster = $('#monster')
 console.log(monster)
 function show() {
-    monster.fadeIn(2000)
+    monster.fadeIn(1000)
 }
 
 function hide() {
-    monster.fadeOut(2000)
+    monster.fadeOut(1000)
 }
 
 function moveRight() {
-
+    monster.animate({left: 500},300)
 }
 
 function moveLeft() {
-
+    monster.animate({left: 500},300)
 }
 
 function moveUp() {
-
+    monster.animate({top: 0},250)
 }
 
 function moveDown() {
-
+    monster.animate({top: 500},300)
 }
 
 function moveSequence() {
