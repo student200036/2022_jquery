ae74f311d77f027d1ff03307660f13d1d78d2c98
student200036 <200036ww@gmail.com>
2022-03-04 10:29:59 +0900

    commit

---
+++ animation/animation.js
@@ -0,0 +1,23 @@
+
+var monster = $('monster')
+console.log(monster)
+function show() {
+}
+
+function hide() {
+}
+
+function moveRight() {
+}
+
+function moveLeft() {
+}
+
+function moveUp() {
+}
+
+function moveDown() {
+}
+
+function moveSequence() {
+}
